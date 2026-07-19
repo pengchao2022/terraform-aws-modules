136ce61e7f0725e748a4dbda45931c713b075386
pengchao2022 <pengchao.ma2@outlook.com>
2026-07-19 20:15:40 +0800

    feat: GTP-54273 added the code

--- iam-user/main.tf
+++ iam-user/main.tf
@@ -1,10 +1,9 @@
-# 创建 IAM 用户
 resource "aws_iam_user" "this" {
   name          = var.name
   force_destroy = var.force_destroy
 }
 
-# 配置控制台登录凭证
+
 resource "aws_iam_user_login_profile" "this" {
   count                   = var.create_console_login ? 1 : 0
   user                    = aws_iam_user.this.name
@@ -12,7 +11,6 @@ resource "aws_iam_user_login_profile" "this" {
   password_reset_required = true
 }
 
-# 创建访问密钥 只有在 var.create_access_key 为 true 时才创建
 resource "aws_iam_access_key" "this" {
   count   = var.create_access_key ? 1 : 0
   user    = aws_iam_user.this.name
